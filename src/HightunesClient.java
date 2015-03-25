15de815895bb977d11adb12b4a762c404488dc4c
Julien René <julien.rene@u-psud.fr>
2015-03-25 14:02:18 +0100

    Progression

---
+++ src/HightunesClient.java
@@ -0,0 +1,61 @@
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+
+public class HightunesClient implements Remote, Runnable {
+	@Override
+	public void run() {
+		//System.setSecurityManager(new SecurityManager());
+		try {
+			Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+			CatalogueInterface catalogue = (CatalogueInterface) registry.lookup("leCatalogue");
+			GestionnairePaniersInterface paniers = (GestionnairePaniersInterface) registry.lookup("lesPaniers");
+			GestionnaireCommandesInterface commandes = (GestionnaireCommandesInterface) registry.lookup("lesCommandes");
+
+			System.out.println("Connecté");
+
+			int idPanier = paniers.creerPanier(), idCommande = -1, choixArticle, qteArticle;
+			String choix;
+			Scanner sc;
+			do {
+				sc = new Scanner(System.in);
+				System.out.println("\nChoix de l'action :\n1) Lister les articles\n2) Ajouter un article au panier\n3) Voir le panier\n4) Confirmer la commande\n5) Quitter");
+				choix = sc.nextLine();
+				switch (choix) {
+				case "1" :
+					System.out.println(catalogue.listeArticles());
+					break;
+				case "2" :
+					System.out.println("Choix de l'article : ");
+					choixArticle = sc.nextInt();
+					paniers.ajouterArticle(idPanier, choixArticle);
+					System.out.println("Quantité : ");
+					qteArticle = sc.nextInt();
+					paniers.modifierQteArticle(idPanier, choixArticle, qteArticle);
+					break;
+				case "3" :
+					if (idPanier != -1)
+						System.out.println(paniers.listeArticles(idPanier));
+					break;
+				case "4" :
+					if (idCommande == -1) {
+						idPanier = -1;
+						//idCommande = paniers.passerCommande(idPanier);
+						//commandes.payer(idCommande);
+						System.out.println("Paiement effectué");
+						choix = "5";
+					}
+					break;
+				default :
+					choix = "5";
+				}
+			} while (choix != "5");
+			sc.close();
+		} catch (Exception e) {
+			System.out.println("Erreur client : " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
